d26c7ef302d074fb48524c9e1e72a202269ddf18
Zaidibeth Ramos <zaidi.ramos@gmail.com>
2023-09-05 12:08:55 -0400

    Initial Setup and Data Loading

--- challenge/model.py
+++ challenge/model.py
@@ -1,6 +1,16 @@
 import pandas as pd
-
 from typing import Tuple, Union, List
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.linear_model import LogisticRegression  # Import necessary for Logistic Regression
+
+from utils import get_period_day, get_min_diff, is_high_season
 
 class DelayModel:
 
@@ -8,12 +18,16 @@ class DelayModel:
         self
     ):
         self._model = None # Model should be saved in this attribute.
+        # Load the data from the CSV file directly in __init__
+        self._data = pd.read_csv("data\data.csv")
 
     def preprocess(
         self,
         data: pd.DataFrame,
         target_column: str = None
-    ) -> Union(Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame):
+    ) -> Union[Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame]:
+        # Tuple[pd.DataFrame, pd.DataFrame] | pd.DataFrame:
+        #Union(Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame):
         """
         Prepare raw data for training or predict.
 
@@ -26,7 +40,31 @@ class DelayModel:
             or
             pd.DataFrame: features.
         """
-        return
+        # Handle missing values (drop rows with missing data)
+        data.dropna(inplace=True)
+
+        features = data.copy()
+
+        # Feature Generation
+        features['period_day'] = features['Fecha-I'].apply(get_period_day)
+        features['high_season'] = features['Fecha-I'].apply(is_high_season)
+        features['min_diff'] = features.apply(get_min_diff, axis=1)
+
+        # Calculate 'delay' feature
+        threshold_in_minutes = 15
+        features['delay'] = np.where(features['min_diff'] > threshold_in_minutes, 1, 0)
+
+        # Check if the target column is specified
+        if target_column is not None:
+            # Split the data into features (X) and the target (y)
+            X = features.drop(columns=[target_column])
+            y = features[target_column]
+
+            # Return features and target as a tuple
+            return X, y
+        else:
+            # If target_column is not specified, return only features
+            return features
 
     def fit(
         self,
@@ -51,7 +89,7 @@ class DelayModel:
 
         Args:
             features (pd.DataFrame): preprocessed data.
-        
+
         Returns:
             (List[int]): predicted targets.
         """
